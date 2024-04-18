50ce927ece3c371869aa1b9298f283c199d809e0
jure412 <jur3curk@gmail.com>
2024-04-18 11:33:16 +0200

    Refactor function name in protected/page.tsx

--- src/app/(protected)/protected/page.tsx
+++ src/app/(protected)/protected/page.tsx
@@ -2,8 +2,9 @@ import { validateRequest } from "../../../../utils/auth";
 import Typography from "../../components/Typography";
 import ClientUser from "../../modules/ClientUser";
 
-export default async function Protected() {
+export default async function Page() {
   const { user } = await validateRequest();
+  // console.log(user);
   return (
     <div className="container mx-auto px-6 py-8">
       <Typography p>{user?.username}</Typography>
