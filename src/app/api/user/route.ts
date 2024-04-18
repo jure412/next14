e413bd65aaeddc8a951c475a95262e76856ea001
jure412 <jur3curk@gmail.com>
2024-04-18 11:37:10 +0200

    Refactor GET method in user route

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,42 +1,8 @@
 import { NextRequest } from "next/server";
 
 export const GET = async (req: NextRequest) => {
-  Response.json({
+  return Response.json({
     success: true,
     msg: ["good"],
   });
-  //   try {
-  //     const { user } = await validateRequest();
-  //     if (user) {
-  //       NextResponse.json({
-  //         success: true,
-  //         msg: ["good"],
-  //         data: user,
-  //       });
-  //     }
-  //     NextResponse.json({
-  //       success: false,
-  //       msg: ["Unauthorized."],
-  //     });
-  //   } catch (error: any) {
-  //     return NextResponse.json({ msg: [error?.message], success: false });
-  //   }
 };
-// export const VerifyUser = async () => {
-//   try {
-//     const { user } = await validateRequest();
-//     if (user) {
-//       return {
-//         success: true,
-//         msg: ["good"],
-//         data: user,
-//       };
-//     }
-//     return {
-//       success: false,
-//       msg: ["Unauthorized."],
-//     };
-//   } catch (error: any) {
-//     return { msg: [error?.message], success: false };
-//   }
-// };
