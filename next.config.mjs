4440b05e3e3a9c5db2982f43974419cbac4901b1
jure412 <jur3curk@gmail.com>
2024-04-18 11:30:39 +0200

    Update next.config.mjs with webpack and images configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals = config.externals || [];
+      config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
+    }
     return config;
+  },
+  images: {
+    domains: ["http://localhost:3000"],
+    unoptimized: true,
   },
 };
 
