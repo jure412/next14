47a3e72766948ebf36cb4ca08dd5d07b7df15ac8
jure412 <jur3curk@gmail.com>
2024-04-26 13:15:48 +0200

    Add drawing actions and validation schemas

---
+++ actions/drawing/index.validation.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const NewDrawingSchema = z.object({
+  name: z.string().min(1, "This field has to be filled"),
+  users: z
+    .array(z.string())
+    .refine(
+      (data) =>
+        data.length === 0 || data.every((item) => typeof item === "string"),
+      {
+        message: "Users must be an array of strings or an empty array",
+      }
+    ),
+});
+
+export const SaveDrawingsSchema = z.object({
+  canvas: z.string(),
+  drawingId: z.string().min(1, "Unvalid drawing id"),
+});
