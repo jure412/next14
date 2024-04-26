ef82be0c27d7489747f8cf13ca35fe36dd7bf2c0
jure412 <jur3curk@gmail.com>
2024-04-26 13:42:51 +0200

    Add server.js and socket.ts files

---
+++ server.js
@@ -0,0 +1,45 @@
+const next = require("next");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer);
+
+  io.on("connection", (socket) => {
+    socket.on("join-room", (roomId) => {
+      socket.join(roomId);
+      socket.to(roomId).emit("user-joined");
+    });
+
+    socket.on("user-joined", (roomId) => {
+      socket.to(roomId).emit("user-joined");
+    });
+
+    socket.on("drawing-line", (roomId, { prevPoint, currentPoint, color }) => {
+      +socket
+        .to(roomId)
+        .emit("drawing-line", { prevPoint, currentPoint, color });
+    });
+
+    socket.on("clear", (roomId) => {
+      socket.to(roomId).emit("clear");
+    });
+  });
+
+  httpServer
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
---
+++ src/socket.ts
@@ -0,0 +1,5 @@
+"use client";
+
+import { io } from "socket.io-client";
+
+export const socket = io(process.env.NEXTAUTH_URL_INTERNAL);
