c1ce7826a3afec0c5f29ed8ec84d9ba11c8b0725
jure412 <jur3curk@gmail.com>
2024-04-26 14:51:47 +0200

    Add Card component

---
+++ src/app/modules/Card/index.tsx
@@ -0,0 +1,36 @@
+import CustomImage from "@/app/components/CustomImage";
+import NextLink from "@/app/components/NextLink";
+import React from "react";
+
+interface CardProps {
+  item: {
+    id: string;
+    url: string;
+    name: string;
+  };
+}
+
+const Card: React.FC<CardProps> = ({ item }) => {
+  return (
+    <NextLink
+      className="bg-white rounded-lg shadow-primary p-4"
+      href={`/drawings/${item.id}`}
+      prefetch
+    >
+      <div>
+        <div className="h-[220px] rounded-xl relative overflow-hidden">
+          <CustomImage
+            src={item.url ? "/" + item.url : "/tree.jpg"}
+            alt={item.name}
+            priority={true}
+            className="object-cover"
+            fill={true}
+          />
+        </div>
+        <h2 className="text-xl font-semibold mt-8">{item.name}</h2>
+      </div>
+    </NextLink>
+  );
+};
+
+export default Card;
