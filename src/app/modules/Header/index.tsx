05dbc7d503edf09627b6bf3bcfd34cb4f441e755
jure412 <jur3curk@gmail.com>
2024-04-26 14:39:02 +0200

    Add client-side code for user authentication and sign out functionality

--- src/app/modules/Header/index.tsx
+++ src/app/modules/Header/index.tsx
@@ -1,33 +1,45 @@
+"use client";
 import Button from "@/app/components/Button";
 import { ButtonSize, ButtonVariant } from "@/app/components/Button/index.types";
 import { LinkVariant } from "@/app/components/Link/index.types";
 import Modal from "@/app/components/Modal";
 import NextLink from "@/app/components/NextLink";
+import { getMe } from "@/app/helpers/queries/index.client";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 import { AiOutlineLogin } from "react-icons/ai";
 import { CiAirportSign1 } from "react-icons/ci";
+import { FaSpinner } from "react-icons/fa";
 import { signOut } from "../../../../actions/auth";
-import { validateRequest } from "../../../../utils/auth";
 import Authentication from "../ModalContent/Authentication";
 
-const Header: React.FC = async () => {
-  const { user } = await validateRequest();
+const Header: React.FC = () => {
+  const queryClient = useQueryClient();
+  const { data, isLoading } = useQuery({
+    queryKey: ["getMe"],
+    queryFn: () => getMe(),
+  });
+
+  const handleSingOut = async () => {
+    await signOut();
+    queryClient.invalidateQueries({ queryKey: ["getMe"] });
+  };
+
   return (
     <header>
-      <div className="h-13 flex items-center justify-between">
+      <div className="h-14 flex items-center justify-between">
         <NextLink variant={LinkVariant.PRIMARY} href="/">
           <CiAirportSign1 size={50} />
         </NextLink>
         <div className="flex gap-8 items-center">
-          {user ? (
+          {isLoading ? (
+            <FaSpinner size={20} className="spinner-icon animate-spin" />
+          ) : data?.data?.user ? (
             <>
               <NextLink variant={LinkVariant.SECONDARY} href="/drawings">
                 Drawings
               </NextLink>
-              <NextLink variant={LinkVariant.SECONDARY} href="/protected">
-                Protected
-              </NextLink>
-              <form action={signOut}>
+              <form action={handleSingOut}>
                 <Button
                   size={ButtonSize.SMALL}
                   variant={ButtonVariant.TERTIARY}
