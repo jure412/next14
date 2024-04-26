0ea88e47cc0f5cacbeafeab9c1327ca75e0ecdd9
jure412 <jur3curk@gmail.com>
2024-04-26 13:16:37 +0200

    Remove unused imports and commented code in auth.ts

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,7 +1,5 @@
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
 import { Adapter, Lucia } from "lucia";
-import { cookies } from "next/headers";
-import { cache } from "react";
 import { prisma } from "../prisma/prismaClient";
 
 const PA: Adapter = new PrismaAdapter(prisma.session, prisma.user);
@@ -9,11 +7,10 @@ const PA: Adapter = new PrismaAdapter(prisma.session, prisma.user);
 export const lucia = new Lucia(PA, {
   sessionCookie: {
     attributes: {
-      // set to `true` when using HTTPS
       secure: process.env.NODE_ENV === "production",
     },
   },
-  getUserAttributes: ({ username, email, profileImageUrl, ...rest }) => {
+  getUserAttributes: ({ username, email, profileImageUrl }) => {
     return {
       username,
       email,
@@ -22,43 +19,6 @@ export const lucia = new Lucia(PA, {
   },
 });
 
-export const validateRequest = cache(async () => {
-  const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
-
-  if (!sessionId)
-    return {
-      user: null,
-      session: null,
-    };
-
-  const { user, session } = await lucia.validateSession(sessionId);
-  try {
-    if (session && session.fresh) {
-      const sessionCookie = lucia.createSessionCookie(session.id);
-      cookies().set(
-        sessionCookie.name,
-        sessionCookie.value,
-        sessionCookie.attributes
-      );
-    }
-    if (!session) {
-      const sessionCookie = lucia.createBlankSessionCookie();
-      cookies().set(
-        sessionCookie.name,
-        sessionCookie.value,
-        sessionCookie.attributes
-      );
-    }
-  } catch {
-    // Next.js throws error when attempting to set cookies when rendering page
-  }
-  return {
-    user,
-    session,
-  };
-});
-
-// IMPORTANT!
 declare module "lucia" {
   interface Register {
     Lucia: typeof lucia;
