b88a7165afa6d09f41fb7edc9eab5094f008a4c5
jure412 <jur3curk@gmail.com>
2024-04-26 14:36:02 +0200

    Add client-side query helper functions

---
+++ src/app/helpers/queries/index.client.ts
@@ -0,0 +1,42 @@
+export const getDrawings = async (
+  url: string = "/api/drawings",
+  options?: RequestInit
+): Promise<any> => {
+  const response = options ? await fetch(url, options) : await fetch(url);
+  const drawings = await response.json();
+  console.log({ drawings });
+  return drawings;
+};
+
+export const getDrawingById = async (
+  id: string,
+  url: string = "/api/drawings/",
+  options?: RequestInit
+): Promise<any> => {
+  const response = options
+    ? await fetch(url + id, options)
+    : await fetch(url + id);
+  const drawing = await response.json();
+  return drawing;
+};
+
+export const getMe = async (
+  url: string = "/api/user",
+  options?: RequestInit
+): Promise<any> => {
+  const response = options ? await fetch(url, options) : await fetch(url);
+  const me = await response.json();
+  return me;
+};
+
+export const getUserById = async (
+  email: string,
+  url: string = "/api/user/",
+  options?: RequestInit
+): Promise<any> => {
+  const response = options
+    ? await fetch(url + email, options)
+    : await fetch(url + email);
+  const user = await response.json();
+  return user;
+};
