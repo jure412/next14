729d7754c573bb1f6a478ca3f150c3e6f7471086
jure412 <jur3curk@gmail.com>
2024-05-07 14:59:10 +0200

    Add middleware checks for /editor route

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,9 +7,15 @@ export async function middleware(req: NextRequest) {
   const cookieExists = cookies().get("auth_session");
   if (
     !cookieExists &&
-    (pathname.startsWith("/drawings") || pathname.startsWith("/api/assets/"))
+    (pathname.startsWith("/drawings") ||
+      pathname.startsWith("/editor") ||
+      pathname.startsWith("/api/assets/"))
   ) {
     return NextResponse.redirect(process.env.NEXTAUTH_URL + "/");
   }
+  // response.headers.append("Set-Cookie", "SameSite=None; Secure");
+  // cookies().set("pathname", pathname);
+  // const requestHeaders = new Headers(req.headers);
+  // requestHeaders.set("x-url", req.url);
   return response;
 }
